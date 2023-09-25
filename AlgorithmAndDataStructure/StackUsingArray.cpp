1e79cb80b9cbc8d9a0a829564fc7954092c93dad
Matin Mohammadi <matinm@student.uia.no>
2023-09-25 23:32:55 +0200

    Stack impleimation using array

--- AlgorithmAndDataStructure/StackUsingArray.cpp
+++ AlgorithmAndDataStructure/StackUsingArray.cpp
@@ -13,65 +13,105 @@ public:
     {
         top = -1;
         length = n;
-        array = new int(n);
+        array = new int[n];
     };
     void addRlement(int data)
     {
-        if(IsFull())
+        if(IsFull())        //check if the stack is full
         {
-            cout<<"The stack is full, not enogh space to a new element.";
+            cout<<"The stack is full, not enogh space to a new element."<<endl;
         }
-        else if(IsEmpty())
+        else   //otherwise
         {
-            top ++;
-            array[top] = data;
-        }
-        else
-        {
-         top ++;
-         array[top] = data;
+            top ++;             //increment the top with one
+            array[top] = data;          //assigning the data to index [top]
         };
     };
 
     int removeElement()
     {
-        int data = top;
+        int data;        //a placeholder for the element we want to remove
 
-        return 0;
+        if(IsEmpty())       //check if there is empty stack.
+        {
+            cout<<"The stack is empty, nothing to remove";
+            return 0;
+        }
+        else
+        {
+            data = array[top];          //assigning the data which is stored in the array [top].
+            top --;                     //decrease the top buy one.
+            return data;              //return the data
+        }
     };
+
 
     bool IsFull()
     {
-        if(top == length)
+        if(top == length-1)   //check the condition, to check if the top is equal to the lengths of the array
         {
             return true;
         }
+        return false;
     };
 
 
     bool IsEmpty()
     {
-        if(top == -1)
+        if(top == -1) //check if the top i equal to the -1
         {
             return true;
         }
+        return false;
     };
 
 
     void Display()
     {
-        if(IsEmpty())
+        if(IsEmpty())   // check if stack is empty
         {
             return;
         }
-        int data = top;
-        while (data != -1)
+        int i = top;        // make a variable and assigning to the top
+        while (i != -1)        // check the condition
         {
-            cout<<array[data]<<endl;
-            data --;
+            cout<<array[i]<<endl; // printing out value of each index in the array
+            i --;       // decrease the i
         }
     }
 };
 
+int main()
+{
+    Stack s(4);
+    s.addRlement(1);
+    s.addRlement(2);
+    s.addRlement(3);
+    s.addRlement(4);
+    s.addRlement(44);
+    s.Display();
+    s.removeElement();
+    cout<<endl;
+    s.Display();
+
+    s.removeElement();
+    cout<<endl;
+    s.Display();
+
+    s.removeElement();
+    cout<<endl;
+    s.Display();
+
+    s.removeElement();
+    cout<<endl;
+    s.Display();
 
 
+    s.removeElement();
+    cout<<endl;
+    s.Display();
+
+
+
+    return 0;
+}
